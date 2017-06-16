12be483e9c670009fa94205b745d30d83bbf0235
Roberts Slisans <rsxdalv@gmail.com>
2017-06-16 10:39:04 +0300

    cleanup

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const program = require("commander");
 const fs_1 = require("fs");
 program
-    .version('v1.0.0')
-    .arguments('<regexp> <replace> <files...>')
+    .version("v1.0.0")
+    .arguments("<regexp> <replace> <files...>")
     .action((regexp, newString, files) => {
-    files.forEach(file => fs_1.readFile(file, 'UTF-8', (err, data) => fs_1.writeFile(file, data.replace(new RegExp(regexp, 'gim'), newString), (writeErr, writeData) => {
+    files.forEach(file => fs_1.readFile(file, "UTF-8", (err, data) => fs_1.writeFile(file, data.replace(new RegExp(regexp, "gim"), newString), (writeErr, writeData) => {
         if (writeErr) {
             console.error(writeErr);
         }
@@ -16,7 +16,5 @@ program
     })));
 })
     .parse(process.argv);
-const stringDD = 'asdds';
-stringDD.replace(new RegExp('', 'gim'), 'adc');
 
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
-/// <reference types='node' />
-import * as chalk from 'chalk';
-import * as program from 'commander';
-import { readFile, writeFile } from 'fs';
+/// <reference types="node" />
+import * as chalk from "chalk";
+import * as program from "commander";
+import { readFile, writeFile } from "fs";
 
 program
-    .version('v1.0.0')
-    .arguments('<regexp> <replace> <files...>')
+    .version("v1.0.0")
+    .arguments("<regexp> <replace> <files...>")
     .action((regexp, newString, files) => {
         files.forEach(file =>
-            readFile(file, 'UTF-8', (err, data) =>
+            readFile(file, "UTF-8", (err, data) =>
                 writeFile(file,
                     data.replace(
-                        new RegExp(regexp, 'gim')
+                        new RegExp(regexp, "gim")
                         , newString),
                     (writeErr, writeData) => {
                         if (writeErr) {
@@ -24,7 +24,3 @@ program
         );
     })
     .parse(process.argv);
-
-const stringDD = 'asdds';
-
-stringDD.replace(new RegExp('', 'gim'), 'adc');
