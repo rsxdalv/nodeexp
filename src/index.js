07026faf0cee9b352c3d0a87665bf2a355b7d709
Roberts Slisans <rsxdalv@gmail.com>
2017-06-16 10:30:27 +0300

    Created program

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const program = require("commander");
+const fs_1 = require("fs");
+program
+    .version("v1.0.0")
+    .arguments("<regexp> <replace> <files...>")
+    .action((regexp, newString, files) => {
+    files.forEach(file => fs_1.readFile(file, "UTF-8", (err, data) => fs_1.writeFile(file, data.replace(new RegExp(regexp, "gim"), newString), (writeErr, writeData) => {
+        if (writeErr) {
+            console.error(writeErr);
+        }
+        else {
+            console.log(writeData);
+        }
+    })));
+})
+    .parse(process.argv);
+const stringDD = "asdds";
+stringDD.replace(new RegExp("", "gim"), "adc");
 
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,29 @@
 import * as chalk from "chalk";
 import * as program from "commander";
 import { readFile, writeFile } from "fs";
+
+program
+    .version("v1.0.0")
+    .arguments("<regexp> <replace> <files...>")
+    .action((regexp, newString, files) => {
+        files.forEach(file =>
+            readFile(file, "UTF-8", (err, data) =>
+                writeFile(file,
+                    data.replace(
+                        new RegExp(regexp, "gim")
+                        , newString),
+                    (writeErr, writeData) => {
+                        if (writeErr) {
+                            console.error(writeErr);
+                        } else {
+                            console.log(writeData);
+                        }
+                    }),
+            ),
+        );
+    })
+    .parse(process.argv);
+
+const stringDD = "asdds";
+
+stringDD.replace(new RegExp("", "gim"), "adc");
